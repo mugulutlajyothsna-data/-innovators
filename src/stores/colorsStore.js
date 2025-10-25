1fa938cbcc56f8eb8806a6604351161c38e34eb8
mugulutlajyothsna-data <mugulutlajyothsna@gmail.com>
2025-10-25 14:20:41 +0530

    color

--- src/stores/colorsStore.js
+++ src/stores/colorsStore.js
@@ -1,9 +1,11 @@
 import { create } from 'zustand';
 import getColors from '../utils/getColors';
 
-export const useColorsStore = create(
-	() => ({
-		colors: getColors('dark'), // Default to dark theme colors
-logInit: () => console.log('🎨 Colors store initialized'),
-	})
-);
+export const useColorsStore = create((set) => ({
+  theme: 'dark',
+  colors: getColors('dark'),
+  setTheme: (newTheme) => {
+    const colors = getColors(newTheme) || getColors('dark');
+    set({ theme: newTheme, colors });
+  },
+}));
